5094adc36cd3e0d37ab7ffbd3b1e651e4a160432
Jang Seonho <92978663+ohnoesganj@users.noreply.github.com>
2024-11-22 01:27:27 +0900

    [Bronze V] Title: 문자와 문자열, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader vs Scanner : 속도 차원에서 br이 더 빠름
+        // BufferedReader => 문자열을 한 줄로 읽음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine()) - 1;
+
+        System.out.println(s.charAt(i));
+    }
+}
