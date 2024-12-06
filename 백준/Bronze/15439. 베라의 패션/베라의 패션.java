08e0d273496c9d76033c770d835a4b8b9701aaf1
Jang Seonho <92978663+ohnoesganj@users.noreply.github.com>
2024-12-06 14:05:01 +0900

    [Bronze IV] Title: 베라의 패션, Time: 68 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append(N * (N-1));
+
+        System.out.println(sb);
+    }
+}
