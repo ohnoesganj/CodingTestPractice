cd77dfebf9ca7383b15bc24bb542829655ba012b
Jang Seonho <92978663+ohnoesganj@users.noreply.github.com>
2024-12-06 14:08:45 +0900

    [Bronze IV] Title: 녹색거탑, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int)Math.pow(2, N));
+
+        System.out.println(sb);
+    }
+}
