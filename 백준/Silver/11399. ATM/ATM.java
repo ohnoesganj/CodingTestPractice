0462ae87d2d46656b68413f6b392d2fa9e5f66d7
Jang Seonho <92978663+ohnoesganj@users.noreply.github.com>
2024-12-24 16:07:24 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11864 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        int sum = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 1; i < N; i++) {
+            arr[i] = arr[i] + arr[i - 1];
+        }
+
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
