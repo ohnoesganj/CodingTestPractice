21ed9a0f3d9eaeaf2b931825e7e7d0418a350bde
Jang Seon Ho <92978663+ohnoesganj@users.noreply.github.com>
2025-01-15 14:20:33 +0900

    [Silver I] Title: 절댓값 힙, Time: 248 ms, Memory: 24284 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 0이 주어진 횟수만큼 답 출력
+        int N = Integer.parseInt(br.readLine());  // 배열의 크기
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return Math.abs(o1) - Math.abs(o2);
+                }
+            }
+        });
+
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if(pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
