e30723ecbd0b9d5bc73d1e318edfb74a58007394
장선호 <92978663+ohnoesganj@users.noreply.github.com>
2025-03-27 11:46:42 +0900

    [Silver II] Title: 유기농 배추, Time: 92 ms, Memory: 13656 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] arr;
+    static boolean[][] visited;
+    static int M, N, K;
+    static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            arr = new int[N][M];
+            visited = new boolean[N][M];
+
+            while (K-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                arr[y][x] = 1;
+            }
+
+            cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!visited[i][j] && arr[i][j] == 1) {
+                        cnt++;
+                        dfs(i, j);
+                    }
+                }
+            }
+
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && arr[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+    }
+
+}
