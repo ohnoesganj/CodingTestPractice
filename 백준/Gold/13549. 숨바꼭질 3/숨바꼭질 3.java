fcef5cb23cfe01f7206b2db380fe9e12dbb5c18c
장선호 <92978663+ohnoesganj@users.noreply.github.com>
2025-04-10 00:18:28 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 188 ms, Memory: 84708 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,42 +1,64 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    public static class Node {
+        int now;
+        int time;
+
+        public Node(int now, int time) {
+            this.now = now;
+            this.time = time;
+        }
+    }
+
+    static boolean[] visited;
+    static int N, K;
     static int max = 100_000;
+    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        boolean[] visited = new boolean[max + 1];
+        visited = new boolean[max + 1];
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{N, 0});
+        bfs();
 
-        int answer = Integer.MAX_VALUE;
+        System.out.println(min);
+    }
 
-        while (!queue.isEmpty()) {
-            int[] tmp = queue.poll();
-            int current = tmp[0];
-            int time = tmp[1];
-            visited[current] = true;
+    public static void bfs() {
+        Queue<Node> q = new LinkedList<>();
 
-            if (current == K) {
-                answer = Math.min(answer, time);
+        q.add(new Node(N, 0));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            visited[cur.now] = true;
+
+            if (cur.now == K) {
+                min = Math.min(min, cur.time);
             }
 
-            if (current * 2 <= max && !visited[current * 2]) {
-                queue.offer(new int[]{current * 2, time});
+            if (cur.now * 2 <= max && !visited[cur.now * 2]) {
+                q.offer(new Node(cur.now * 2, cur.time));
             }
-            if (current + 1 <= max && !visited[current + 1]) {
-                queue.offer(new int[]{current + 1, time + 1});
+            if (cur.now + 1 <= max && !visited[cur.now + 1]) {
+                q.offer(new Node(cur.now + 1, cur.time + 1));
             }
-            if (current - 1 >= 0 && !visited[current - 1]) {
-                queue.offer(new int[]{current - 1, time + 1});
+            if (cur.now - 1 >= 0 && !visited[cur.now - 1]) {
+                q.offer(new Node(cur.now - 1, cur.time + 1));
             }
         }
-        System.out.println(answer);
     }
 }
